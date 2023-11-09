f9017563faa0bbb229e6ff8ad1873df6c5528e42
lqinggang <1944058861@qq.com>
2023-11-09 19:37:28 +0800

    demo

---
+++ README.md
@@ -0,0 +1,14 @@
+---
+home: true
+heroText: Linux 学习笔记
+tagline: Linux 学习笔记
+actionText: 快速上手
+actionLink: /
+features:
+- title: 设备驱动
+  details: 设备驱动
+- title: 文件系统
+  details: 文件系统
+- title: 进程/线程同步
+  details: 进程/线程同步
+---
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,43 @@
+module.exports = {
+    base: '/myLinuxBook/',
+    title: 'Linux 学习笔记',
+    description: 'Linux 学习笔记',
+    plugins: [
+        [ 'vuepress-plugin-side-anchor' ]
+    ],
+    themeConfig: {
+        repo: 'https://github.com/Lqinggang',
+        repoLabel: 'GitHub',
+        smoothScroll: true,
+        displayAllHeaders: true,
+        nav: [
+            { text: 'Home', link: '/' }
+        ],
+        sidebar: [
+            [ '/', '首页' ],
+            {
+                title: '设备驱动',
+                children: [
+                    [ '/driver/i2c.md', 'I2C 设备' ],
+                    [ '/driver/spi.md', 'SPI 设备' ]
+                ]
+            },
+            {
+                title: '文件系统',
+                children: [ '/fs/' ]
+            },
+            {
+                title: '网络通信',
+                children: [ '/network/' ]
+            },
+            {
+                title: '进程/线程间同步',
+                children: [ '/sync/' ]
+            },
+            {
+                title: '中断和异常',
+                children: [ '/interrupt/' ]
+            }
+        ]
+    }
+}
